1c092662ddab4a95a08279d1ea2b987c825a13aa
takuseno <takuma.seno@gmail.com>
2020-07-01 11:23:46 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name="d4rl_atari",
+      version="0.1",
+      license="MIT",
+      description="Datasets for data-driven deep reinforcement learnig with Atari (wrapper for datasets released by Google)",
+      url="https://github.com/takuseno/d4rl-atari",
+      install_requires=["atari-py", "gym", "gsutil", "numpy", "opencv-python"],
+      packages=find_packages())
