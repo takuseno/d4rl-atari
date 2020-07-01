49ca7b768f7461d5ff971bae3559cbcead7e907c
takuseno <takuma.seno@gmail.com>
2020-07-01 12:10:26 +0900

    Skip dataset test on CI

--- d4rl_atari/tests/test_env.py
+++ d4rl_atari/tests/test_env.py
@@ -1,19 +1,21 @@
 import gym
 import d4rl_atari
+import os
 
 
 def test_env():
     env = gym.make('breakout-mixed-v0')
-
-    dataset = env.get_dataset()
-
-    assert dataset['observations'].shape == (1000000, 84, 84)
-    assert dataset['actions'].shape == (1000000, )
-    assert dataset['rewards'].shape == (1000000, )
-    assert dataset['terminals'].shape == (1000000, )
 
     observation = env.reset()
     assert observation.shape == (84, 84, 1)
 
     observation = env.step(1)[0]
     assert observation.shape == (84, 84, 1)
+
+    # skip this on CI due to memory allocation error
+    if not os.getenv('CI'):
+        dataset = env.get_dataset()
+        assert dataset['observations'].shape == (1000000, 84, 84)
+        assert dataset['actions'].shape == (1000000, )
+        assert dataset['rewards'].shape == (1000000, )
+        assert dataset['terminals'].shape == (1000000, )
