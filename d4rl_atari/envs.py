e973ff8eb07b86e10b4ff621872986541241e085
takuseno <takuma.seno@gmail.com>
2020-07-01 09:58:53 +0900

    Initial commit

---
+++ d4rl_atari/envs.py
@@ -0,0 +1,69 @@
+import numpy as np
+import gym
+import cv2
+
+from gym import spaces
+from .offline_env import OfflineEnv
+
+
+def capitalize_game_name(game):
+    game = game.replace('-', '_')
+    return ''.join([g.capitalize() for g in game.split('_')])
+
+
+class AtariEnv(gym.Env):
+    def __init__(self, game, frameskip=4, **kwargs):
+        # set action_probability=0.25
+        env_id = '{}NoFrameskip-v0'.format(game)
+        atari_env = gym.make(env_id)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(84, 84, 1), dtype=np.uint8)
+        self.action_space = atari_env.action_space
+        self.env = atari_env.env
+
+        self.frameskip = frameskip
+        self.screen_buffer = np.zeros((2, 84, 84), dtype=np.uint8)
+
+    def reset(self):
+        self.env.reset()
+        self._fetch_grayscale_observation(self.screen_buffer[0])
+        self.screen_buffer[1].fill(0)
+        return self._pool_and_resize()
+
+    def step(self, action):
+        accumulated_reward = 0.0
+        for time_step in range(self.frameskip):
+            _, reward, done, info = self.env.step(action)
+
+            accumulated_reward += reward
+
+            if done:
+                break
+            elif time_step >= self.frameskip - 2:
+                t = time_step - (self.frameskip - 2)
+                self._fetch_grayscale_observation(self.screen_buffer[t])
+
+        observation = self._pool_and_resize()
+
+        return observation, accumulated_reward, done, info
+
+    def _fetch_grayscale_observation(self, output):
+        self.env.ale.getScreenGrayscale(output)
+        return output
+
+    def _pool_and_resize(self):
+        np.maximum(self.screen_buffer[0], self.screen_buffer[1], out=self.screen_buffer[0])
+
+        resized_screen = cv2.resize(self.screen_buffer[0], (84, 84), interpolation=cv2.INTER_AREA)
+
+        image = np.asarray(resized_screen, dtype=np.uint8)
+
+        return np.expand_dims(image, axis=2)
+
+
+
+class OfflineAtariEnv(AtariEnv, OfflineEnv):
+    def __init__(self, **kwargs):
+        game = capitalize_game_name(kwargs['game'])
+        del kwargs['game']
+        AtariEnv.__init__(self, game=game, **kwargs)
+        OfflineEnv.__init__(self, game=game, **kwargs)
---
+++ d4rl_atari/offline_env.py
@@ -0,0 +1,118 @@
+import numpy as np
+import os
+import gym
+import gzip
+
+from subprocess import Popen 
+
+
+URI = 'gs://atari-replay-datasets/dqn/{}/{}/replay_logs/'
+BASE_DIR = os.path.join(os.environ['HOME'], '.d4rl', 'datasets')
+
+
+def get_dir_path(env, index, epoch, base_dir=BASE_DIR):
+    return os.path.join(base_dir, env, str(index), str(epoch))
+
+
+def inspect_dir_path(env, index, epoch, base_dir=BASE_DIR):
+    path = get_dir_path(env, index, epoch, base_dir)
+    if not os.path.exists(path):
+        return False
+    for name in ['observation', 'action', 'reward', 'terminal']:
+        if not os.path.exists(os.path.join(path, name + '.npy')):
+            return False
+    return True
+
+
+def unzip(path):
+    with gzip.open(path, 'rb') as gzip_f:
+        npy_path = path.replace('gz', 'npy')
+        with open(npy_path, 'wb') as f:
+            # read in chunks otherwise OSError occurs
+            while True:
+                chunk = gzip_f.read(1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+
+
+def _download(name, env, index, epoch, dir_path):
+    file_name = '$store$_{}_ckpt.{}.gz'.format(name, epoch)
+    uri = URI.format(env, index) + file_name
+    path = os.path.join(dir_path, '{}.gz'.format(name))
+    p = Popen(['gsutil', '-m', 'cp', '-R', uri, path])
+    p.wait()
+    return path
+
+
+def download_dataset(env, index, epoch, base_dir=BASE_DIR):
+    dir_path = get_dir_path(env, index, epoch, base_dir)
+
+    # download observation
+    observation_path = _download('observation', env, index, epoch, dir_path)
+    print('decompressing observation.gz...')
+    unzip(observation_path)
+
+    # download action
+    action_path = _download('action', env, index, epoch, dir_path)
+    print('decompressing action.gz...')
+    unzip(action_path)
+
+    # download reward
+    reward_path = _download('reward', env, index, epoch, dir_path)
+    print('decompressing reward.gz...')
+    unzip(reward_path)
+
+    # download terminal
+    terminal_path = _download('terminal', env, index, epoch, dir_path)
+    print('decompressing terminal.gz...')
+    unzip(terminal_path)
+
+
+class OfflineEnv(gym.Env):
+    def __init__(self,
+                 game=None,
+                 index=None,
+                 start_epoch=None,
+                 last_epoch=None,
+                 **kwargs):
+        super(OfflineEnv, self).__init__(**kwargs)
+        self.game = game
+        self.index = index
+        self.start_epoch = start_epoch
+        self.last_epoch = last_epoch
+
+    def get_dataset(self):
+        observation_stack = []
+        action_stack = []
+        reward_stack = []
+        terminal_stack = []
+        for epoch in range(self.start_epoch, self.last_epoch + 1):
+            path = get_dir_path(self.game, self.index, epoch)
+            if not inspect_dir_path(self.game, self.index, epoch):
+                os.makedirs(path, exist_ok=True)
+                download_dataset(self.game, self.index, epoch)
+
+            observations = np.load(os.path.join(path, 'observation.npy'))
+            actions = np.load(os.path.join(path, 'action.npy'))
+            rewards = np.load(os.path.join(path, 'reward.npy'))
+            terminals = np.load(os.path.join(path, 'terminal.npy'))
+
+            assert observations.shape == (1000000, 84, 84)
+            assert actions.shape == (1000000,)
+            assert rewards.shape == (1000000,)
+            assert terminals.shape == (1000000,)
+
+            observation_stack.append(observations)
+            action_stack.append(actions)
+            reward_stack.append(rewards)
+            terminal_stack.append(terminals)
+
+        data_dict = {
+            'observations': np.vstack(observation_stack),
+            'actions': np.vstack(action_stack),
+            'rewards': np.vstack(reward_stack),
+            'terminals': np.vstack(terminal_stack)
+        }
+
+        return data_dict
