0e70ac88ee1463aa9eca446aa783becd1d219547
takuseno <takuma.seno@gmail.com>
2023-07-21 17:14:00 +0900

    Revert "Fix wrapping environment"
    This reverts commit bec9228301081644decadbab951362dd99d068ed.

--- d4rl_atari/envs.py
+++ d4rl_atari/envs.py
@@ -1,6 +1,8 @@
 import numpy as np
 import gym
+import cv2
 
+from gym import spaces
 from gym.wrappers import AtariPreprocessing, TransformReward, FrameStack
 from .offline_env import OfflineEnv
 
@@ -10,7 +12,7 @@ def capitalize_game_name(game):
     return ''.join([g.capitalize() for g in game.split('_')])
 
 
-class AtariEnv(gym.Wrapper):
+class AtariEnv(gym.Env):
 
     def __init__(self,
                  game,
@@ -23,7 +25,7 @@ class AtariEnv(gym.Wrapper):
         env_id = '{}NoFrameskip-v{}'.format(game, 0 if sticky_action else 4)
 
         # use official atari wrapper
-        env = AtariPreprocessing(gym.make(env_id, **kwargs),
+        env = AtariPreprocessing(gym.make(env_id),
                                  terminal_on_life_loss=terminal_on_life_loss)
 
         if stack:
@@ -33,7 +35,22 @@ class AtariEnv(gym.Wrapper):
             env = TransformReward(env, lambda r: np.clip(r, -1.0, 1.0))
 
         self._env = env
-        super().__init__(env)
+
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+
+    def step(self, action):
+        return self._env.step(action)
+
+    def reset(self):
+        return self._env.reset()
+
+    def render(self, mode='human'):
+        self._env.render(mode)
+
+    def seed(self, seed=None):
+        super().seed(seed)
+        self._env.seed(seed)
 
 
 class OfflineAtariEnv(AtariEnv, OfflineEnv):
--- d4rl_atari/offline_env.py
+++ d4rl_atari/offline_env.py
@@ -71,7 +71,7 @@ def _stack(observations, terminals, n_channels=4):
     return rets
 
 
-class OfflineEnv:
+class OfflineEnv(gym.Env):
 
     def __init__(self,
                  game=None,
@@ -80,6 +80,7 @@ class OfflineEnv:
                  last_epoch=None,
                  stack=False,
                  **kwargs):
+        super(OfflineEnv, self).__init__(**kwargs)
         self.game = game
         self.index = index
         self.start_epoch = start_epoch
